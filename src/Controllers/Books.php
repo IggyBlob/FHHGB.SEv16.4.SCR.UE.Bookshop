b7afb9feac2a2504a036d2b63968c3aa347b3085
IggyBlob <paul.haunschmied@students.fh-hagenberg.at>
2018-04-20 15:38:20 +0200

    Add BookList table output

--- src/Controllers/Books.php
+++ src/Controllers/Books.php
@@ -15,7 +15,7 @@
         public function GET_Index() {
             return $this->renderView('booklist', array(
                 'categories' => $this->dataLayer->getCategories(),
-                'seletedCategoryId' => $this->getParam(self::PARAM_CATEGORY_ID),
+                'selectedCategoryId' => $this->getParam(self::PARAM_CATEGORY_ID),
                 'books' => $this->hasParam(self::PARAM_CATEGORY_ID) ?
                     $this->dataLayer->getBooksForCategory($this->getParam(self::PARAM_CATEGORY_ID)) : null
             ));
